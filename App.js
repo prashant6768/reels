2ced84de3fd7bac94eaa0265ff2ae4aeae75eb65
Prashant Hanamghar <prashant.hanamghar@gmail.com>
2023-03-31 18:12:15 +0530

    swiper test

--- App.js
+++ App.js
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import * as React from 'react';
+// import { View, StyleSheet, Button } from 'react-native';
+// import { Video, AVPlaybackStatus } from 'expo-av';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// export default function App() {
+//   const video = React.useRef(null);
+//   const [status, setStatus] = React.useState({});
+//   return (
+//     <View style={styles.container}>
+//       <Video
+//         ref={video}
+//         style={styles.video}
+//         source={{
+//           uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+//         }}
+//         useNativeControls
+//         resizeMode="cover"
+//         isLooping
+//         // shouldPlay
+//         onPlaybackStatusUpdate={status => setStatus(() => status)}
+//       />
+//     </View>
+//   );
+// }
 
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'black',
+//   },
+//   video: {
+//     position: 'absolute',
+//     top: 0,
+//     right: 0,
+//     bottom: 0,
+//     left: 0
+//   }
+// });
+
+
+import React from 'react';
+import { Text, Dimensions, StyleSheet, View } from 'react-native';
+import { SwiperFlatList } from 'react-native-swiper-flatlist';
+
+const colors = ['tomato', 'thistle', 'skyblue', 'teal'];
+
+const App = () => (
+  <View style={styles.container}>
+    <SwiperFlatList
+      autoplay
+      autoplayDelay={2}
+      autoplayLoop
+      index={2}
+      showPagination
+      data={colors}
+      renderItem={({ item }) => (
+        <View style={[styles.child, { backgroundColor: item }]}>
+          <Text style={styles.text}>{item}</Text>
+        </View>
+      )}
+    />
+  </View>
+);
+
+const { width } = Dimensions.get('window');
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { flex: 1, backgroundColor: 'white' },
+  child: { width, justifyContent: 'center' },
+  text: { fontSize: width * 0.5, textAlign: 'center' },
 });
+
+export default App;
