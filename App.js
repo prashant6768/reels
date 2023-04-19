070178cbcbb0ca47f564286442c83bc9d3873d0b
prashant6768 <prashant.hanamghar@gmail.com>
2023-04-20 02:30:58 +0530

    Initial

--- App.js
+++ App.js
@@ -1,34 +1,85 @@
 import * as React from 'react';
-import { View, Text, StyleSheet, Button, SafeAreaView } from 'react-native';
+import { View, Text, StyleSheet, Button, SafeAreaView, Image, Touchable, TouchableOpacity } from 'react-native';
 import { Video, AVPlaybackStatus } from 'expo-av';
 import { colors } from 'react-native-swiper-flatlist/src/themes';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { StatusBar } from 'expo-status-bar';
+import { data } from 'data';
+import { SwiperFlatList } from 'react-native-swiper-flatlist';
+import { FlatList } from 'react-native';
 
 export default function App() {
   const video = React.useRef(null);
   const [status, setStatus] = React.useState({});
+  const renderItem = () => {
+    return (
+      <View>
+        <Text>Ddddddd</Text>
+      </View>
+    )
+  }
   return (
     <View style={styles.container}>
-      <StatusBar style='light'/>
+      <StatusBar style='light' />
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+      />
       <Video
         ref={video}
         style={styles.backgroundVideo}
         source={{
           uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
         }}
-        useNativeControls
+        // useNativeControls
         resizeMode="cover"
         isLooping
         // shouldPlay
         onPlaybackStatusUpdate={status => setStatus(() => status)}
       />
-      <SafeAreaView style={{margin:16}}>
+      <SafeAreaView style={{ margin: 16 }}>
         <View style={styles.flexHorizontal}>
           <Text style={styles.textStyle}>Reels</Text>
-          <Ionicons name="camera-outline" size={32} color="white" />
+          <Ionicons name="camera-outline" size={28} color="white" />
         </View>
       </SafeAreaView>
+      <View style={styles.bottomView}>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <Image
+            source={{ uri: "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?cs=srgb&dl=pexels-pixabay-220453.jpg&fm=jpg" }}
+            style={styles.profileImageStyle}
+          />
+          <Text style={{ marginHorizontal: 8 }}>Prashant Hanamghar</Text>
+          <TouchableOpacity>
+            <Text>Follow</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{ flexDirection: 'row', marginTop: 8 }}>
+          <Text numberOfLines={1} style={{ flex: 1 }}>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</Text>
+          <TouchableOpacity>
+            <Text>more</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{ ...styles.flexHorizontal, marginVertical: 8 }}>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Ionicons name="heart-outline" size={28} color="white" />
+            <Ionicons style={{ marginHorizontal: 8 }} name="chatbubble-outline" size={28} color="white" />
+            <Ionicons name="paper-plane-outline" size={28} color="white" />
+            <Ionicons style={{ marginHorizontal: 8 }} name="bookmark-outline" size={28} color="white" />
+            <Ionicons name="ellipsis-horizontal" size={28} color="white" />
+          </View>
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Ionicons name="heart" size={15} color="white" />
+              <Text style={{ marginHorizontal: 4 }}>94.6k</Text>
+            </View>
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Ionicons name="chatbubble" size={15} color="white" />
+              <Text style={{ marginLeft: 4 }}>112</Text>
+            </View>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
@@ -54,6 +105,17 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     color: 'white'
+  },
+  bottomView: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingVertical: 32,
+    paddingHorizontal: 16
+  },
+  profileImageStyle: {
+    height: 40,
+    width: 40,
+    borderRadius: 20
   }
 });
 
---
+++ data.js
@@ -0,0 +1,37 @@
+export const data = [
+    {
+        description: "Lorem ipsum dolor sit amet. Qui perferendis repellat aut facere voluptatem aut consectetur consequatur et",
+        url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        subtitle: "By Blender Foundation",
+        thumb: "images/BigBuckBunny.jpg",
+        title: "Big Buck Bunny"
+    },
+    {
+        description: "dolorum officia non eius saepe! Qui minus delectus et exercitationem ratione qui impedit",
+        url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+        subtitle: "By Google",
+        thumb: "images/ForBiggerBlazes.jpg",
+        title: "For Bigger Blazes"
+    },
+    {
+        description: "porro est voluptatum rerum qui ipsam error et alias ipsa id Quis fugiat.",
+        url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4",
+        subtitle: "By Google",
+        thumb: "images/ForBiggerJoyrides.jpg",
+        title: "For Bigger Joyrides"
+    },
+    {
+        description: "Sed quia doloremque ut esse aperiam ut nulla veritatis et sapiente fuga et distinctio galisum sed adipisci officia et nisi natus.",
+        url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",
+        subtitle: "By Google",
+        thumb: "images/ForBiggerMeltdowns.jpg",
+        title: "For Bigger Meltdowns"
+    },
+    {
+        description: "Qui quos minima eos beatae aliquam et galisum Quis ut fuga recusandae.",
+        url: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4",
+        subtitle: "By Blender Foundation",
+        thumb: "images/Sintel.jpg",
+        title: "Sintel"
+    },
+]
